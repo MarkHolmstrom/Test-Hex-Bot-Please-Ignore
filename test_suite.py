ca6579bea93a49d3b07e10f0d9ecf2542a72b803
Steven Tang <18170455+steventango@users.noreply.github.com>
2023-02-20 23:44:30 -0700

    Revert "Merge branch 'master' of github.com:MarkHolmstrom/Test-Hex-Bot-Please-Ignore"
    This reverts commit ffb3c80e6611621814fc884c46fd17756be6c3ef, reversing
    changes made to fa375dfecd742e5a608526a9ea3c66c229e2a06a.

--- test_suite.py
+++ test_suite.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Script for testing bots. Error handling is lazy, if something breaks then it'll probably break immediately.
+Script for testing bots. Note that wins are reported as original player,
+e.g. if swap happens and "Black" wins, it counts as a win for White.
 """
 import argparse
 import multiprocessing
@@ -10,8 +11,8 @@ import time
 
 RUST_PREAMBLE = "cargo run --bin central_controller --release -- matchup"
 BOARD_SIZE = 10
-BLACK_PATH = "../../hexbot/main.out"
-WHITE_PATH = "../../hexbot/main.out"
+BLACK_PATH = "../../hexbot/cpp_bot/main.out"
+WHITE_PATH = "../../hexbot/cpp_bot/main.out"
 RUN_TIME_SECS = 20
 
 
@@ -26,10 +27,6 @@ def play_games(args, black_wins, white_wins, start_time, run_time):
 
         p.stdin.write("run " + str(BOARD_SIZE ** 2 + 1))  # swap move and fill board
         p.stdin.close()
-        for line in p.stderr:  # stderr has a bunch of rust warnings
-            if "thread 'main' panicked" in line:
-                print(line)
-                return
 
         swap = 0
         for line in p.stdout:
@@ -54,7 +51,7 @@ def main():
     parser.add_argument('-s', default=BOARD_SIZE,
                         help="Board size (side length)")
     parser.add_argument('-t', default=RUN_TIME_SECS,
-                        help="Run time (seconds, total), script will wait for games to finish")
+                        help="Run time (seconds), script will not start new games after this time has elapsed")
     parser.add_argument('-l', default="",
                         help="Log path (will log win counts to path)")
 
@@ -83,4 +80,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
